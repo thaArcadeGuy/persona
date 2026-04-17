e950539515e002fae78f0c1c20fe3b098aa4cf1c
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-04-18 00:07:33 +0300

    add a console log to your db.js to see if it's even attempting to connect

--- config/db.js
+++ config/db.js
@@ -5,10 +5,12 @@ const mongoUri = process.env.MONGO_URI
 
 exports.connectDB = async () => {
   try {
-    await mongoose.connect(mongoUri)
-    console.log("Database connection was successful")
+    console.log("Attempting to connect to MongoDB...");
+    console.log("Connection string exists?", !!mongoUri);
+    await mongoose.connect(mongoUri);
+    console.log("Database connection was successful");
   } catch (error) {
-    console.error("Error connecting to database", error)
-    process.exit(1)
+    console.error("Error connecting to database", error);
+    process.exit(1);
   }
-}
+};
