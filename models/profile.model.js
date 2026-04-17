e683a4ba8eb356282cf126237280e3572e12ae77
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-04-17 18:05:22 +0300

    Set UUID v7 as the actual MongoDB primary key

--- models/profile.model.js
+++ models/profile.model.js
@@ -1,18 +1,57 @@
-const mongoose = require("mongoose")
-const { generate } = require("@babia/uuid-v7/generate")
+const mongoose = require("mongoose");
+const { generate } = require("@babia/uuid-v7/generate");
 
-const profileSchema = new mongoose.Schema({
-  id: { type: String, default: () => generate() },
-  name: { type: String, lowercase: true, index: true },
-  gender: { type: String , index: true},
-  gender_probability: { type: Number },
-  sample_size: { type: Number },
-  age: { type: Number },
-  age_group: { type: String, index: true },
-  country_id: { type: String, index: true },
-  country_probability: { type: Number },
-  created_at: { type: Date, default: () => new Date() }
-})
+const profileSchema = new mongoose.Schema(
+  {
+    _id: { 
+      type: String, 
+      default: () => generate() 
+    },
+    name: { 
+      type: String, 
+      lowercase: true, 
+      index: true 
+    },
+    gender: { 
+      type: String, 
+      index: true 
+    },
+    gender_probability: { 
+      type: Number 
+    },
+    sample_size: { 
+      type: Number 
+    },
+    age: { 
+      type: Number 
+    },
+    age_group: { 
+      type: String, 
+      index: true 
+    },
+    country_id: { 
+      type: String, 
+      index: true 
+    },
+    country_probability: { 
+      type: Number 
+    },
+    created_at: { 
+      type: Date, 
+      default: () => new Date() 
+    },
+  },
+  {
+    _id: false,
+    versionKey: false,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret.id;
+        delete ret._id;
+        return ret;
+      },
+    },
+  },
+);
 
-
-module.exports = mongoose.model("Profile", profileSchema)
+module.exports = mongoose.model("Profile", profileSchema);
