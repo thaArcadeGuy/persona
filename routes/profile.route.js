818d67cc9c2585888ca98108649c24ccfbddc60a
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-04-27 20:47:21 +0300

    feat(auth): add the auth and role middleware to each route

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const apiRouter = express.Router()
 const profileController = require("../controllers/profile.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
+const roleCheck = require("../middlewares/roles.middleware")
 
-apiRouter.post("/profiles", profileController.createProfile)
-apiRouter.get("/profiles", profileController.getAllProfiles)
-apiRouter.get("/profiles/search", profileController.searchProfiles)
-apiRouter.get("/profiles/:id", profileController.getProfileById)
-apiRouter.delete("/profiles/:id", profileController.deleteProfile)
+apiRouter.post("/profiles", authMiddleware, roleCheck("admin"), profileController.createProfile)
+apiRouter.get("/profiles", authMiddleware, roleCheck("admin", "analyst"), profileController.getAllProfiles)
+apiRouter.get("/profiles/search", authMiddleware, roleCheck("admin", "analyst"), profileController.searchProfiles)
+apiRouter.get("/profiles/:id", authMiddleware, roleCheck("admin", "analyst"), profileController.getProfileById)
+apiRouter.delete("/profiles/:id", authMiddleware, roleCheck("admin"), profileController.deleteProfile)
 
 module.exports = apiRouter
