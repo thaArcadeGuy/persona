5e5f585482692d24542f503b6060256c5881f560
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-04-27 14:22:32 +0300

    feat(auth): add refresh token flow with reuse prevention and user validation

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,9 @@ const express = require("express")
 const apiRouter = express.Router()
 const passport = require("passport")
 const { generateAccessToken, generateRefreshToken } = require("../utils/generateTokens")
+const jwt = require("jsonwebtoken")
+const BlockedToken = require("../models/blockedToken.model")
+const User = require("../models/user.model")
 
 apiRouter.get("/github", passport.authenticate("github", { scope: [ "user:email" ] }))
 
@@ -35,6 +38,82 @@ apiRouter.get("/github/callback", (req, res, next) => {
       }
     })
   })(req, res, next)
+})
+
+apiRouter.post("/refresh", async (req, res, next) => {
+  const token = req.body.refresh_token
+
+  if (!token) {
+    return res.status(400).json({
+      status: "error",
+      message: "Refresh token is required"
+    })
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const exists = await BlockedToken.findOne({ token })
+
+    if (exists) {
+      return res.status(403).json({
+        status: "error",
+        message: "Token already used"
+      })
+    }
+
+    const user = await User.findOne({ _id: decoded.id }) 
+
+    if (!user) {
+      return res.status(404).json({ 
+        status: "error",
+        message: "User not found" 
+      });
+    }
+
+    if (!user.is_active) {
+      return res.status(403).json({
+        status: "error",
+        message: "Account is deactivated"
+      })
+    }
+
+    
+    await BlockedToken.create({
+      token: token,
+      expiresAt: new Date(decoded.exp * 1000)
+    })
+
+
+    const accessToken = generateAccessToken(user)
+    const refreshToken = generateRefreshToken(user)
+
+    res.status(200).json({
+      status: "success",
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    })
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      const expiredAt = error.expiredAt
+      return res.status(401).json({
+        status: "error",
+        message: `Token expired at: ${expiredAt}`
+      })
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        status: "error",
+        message: "Invalid token"
+      })
+    }
+
+    res.status(500).json({
+      status: "error",
+      message: "Failed to get refresh token",
+    });
+  }
 })
 
 module.exports = apiRouter
