c65a46cd4cdff262b48149a317574abec3d5f916
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-04-30 01:04:05 +0300

    fix(auth): update the callback url

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ passport.use(
   new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/auth/github/callback"
+    callbackURL: "https://persona-ruddy.vercel.app/auth/github/callback"
   },
    async function(accessToken, refreshToken, profile, done) {
     try {
